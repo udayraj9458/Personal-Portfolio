dedb51133c5982c8ce1e1007623c26e95e359152
Udayraj Narale <128830791+udayraj9458@users.noreply.github.com>
2024-10-04 13:55:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// toggle icon menu
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+// Scroll section active link
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(linls => {
+                linls.classList.remove('active');
+                document.querySelector('header nav a[href*=' +id +']').classList.add('active');
+            });
+        };
+    });
+
+    // sticky navbar
+
+    let header =document.querySelector('header');
+
+    header.classList.toggle('sticky', window,scrollY > 100)
+
+    //remove toggle icon & navbar when click navbar link
+
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+};
